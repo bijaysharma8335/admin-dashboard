d7f0dfa1230fb06781b46aaca200df4ce422f0ae
Bijay Sharma <bijaysharma8335@gmail.com>
2023-07-05 21:24:11 +0530

    Install required dependencies
    -Create dummy constant file
    -Create Ecommerce Page
    -Remove unnecessary files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Ecommerce from "./pages/Ecommerce";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    return (
+        <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Ecommerce/>}/>
+        </Routes>
+        </BrowserRouter>
+    );
+};
 
 export default App;
