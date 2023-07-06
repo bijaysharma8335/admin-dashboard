37f9f294f9fe24470c7f60ad59d3df692c9e7926
Bijay Sharma <bijaysharma8335@gmail.com>
2023-07-06 23:22:52 +0530

    Update Content in Ecommerce Page

--- src/pages/Ecommerce.jsx
+++ src/pages/Ecommerce.jsx
@@ -4,9 +4,15 @@ import { GoPrimitiveDot } from "react-icons/go";
 
 import { useStateContext } from "../context/ContextProvider";
 import { DropDownListComponent } from "@syncfusion/ej2-react-dropdowns";
-import { SparklineAreaData, dropdownData, earningData, ecomPieChartData } from "../data/dummy";
-import Button from "../components/Button";
-import { Pie, SparkLine, Stacked } from "../components";
+import {
+    SparklineAreaData,
+    dropdownData,
+    earningData,
+    ecomPieChartData,
+    recentTransactions,
+} from "../data/dummy";
+
+import { Pie, SparkLine, Stacked, Button, LineChart } from "../components";
 
 const DropDown = (currentMode) => {
     <div className="w-28 border-1 border-color px-2 py-1 rounded-md">
@@ -171,9 +177,64 @@ const Ecommerce = () => {
                     </div>
                 </div>
             </div>
-
-
             {/* 201 line no */}
+            <div className="flex gap-10 m-4 flex-wrap justify-center">
+                <div className="bg-white dark:text-gray-200 dark:bg-secondary-dark-bg p-6 rounded-2xl">
+                    <div className="flex justify-between items-center gap-2">
+                        <p className="text-xl font-semibold">Recent Transactions</p>
+                        <DropDown currentMode={currentMode} />
+                    </div>
+                    <div className="mt-10 w-72 md:w-400">
+                        {recentTransactions.map((item) => (
+                            <div key={item.title} className="flex justify-between mt-4">
+                                <div className="flex gap-4">
+                                    <button
+                                        type="button"
+                                        style={{
+                                            color: item.iconColor,
+                                            backgroundColor: item.iconBg,
+                                        }}
+                                        className="text-2xl rounded-lg p-4 hover:drop-shadow-xl"
+                                    >
+                                        {item.icon}
+                                    </button>
+                                    <div>
+                                        <p className="text-md font-semibold">{item.title}</p>
+                                        <p className="text-sm text-gray-400">{item.desc}</p>
+                                    </div>
+                                </div>
+                                <p className={`text-${item.pcColor}`}>{item.amount}</p>
+                            </div>
+                        ))}
+                    </div>
+
+                    <div className="flex justify-between items-center mt-5 border-t-1 border-color">
+                        <div className="mt-3">
+                            <Button
+                                color="white"
+                                bgColor={currentColor}
+                                text="Add"
+                                borderRadius="10px"
+                            />
+                        </div>
+
+                        <p className="text-gray-400 text-sm">36 Recent Transactions</p>
+                    </div>
+                </div>
+                <div className="bg-white dark:text-gray-200 dark:bg-secondary-dark-bg p-6 rounded-2xl w-96  m:w-760 ">
+                    <div className="flex justify-between items-center gap-2 mb-10">
+                        <p className="text-xl font-semibold">Sales Overview</p>
+                        <DropDown currentMode={currentMode} />
+                    </div>
+                    <div className="md:w-full overflow-auto">
+                        <LineChart />
+                    </div>
+                </div>
+            </div>
+
+            <div className="flex flex-wrap justify-center">
+                <div className="md:w-400 bg-white dark:text-gray-200"> </div>
+            </div>
         </div>
     );
 };
