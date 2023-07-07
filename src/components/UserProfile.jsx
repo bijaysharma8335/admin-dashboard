0389b1e504b3027a9c44af3075a38c62d008d877
Bijay Sharma <58084675+bijaysharma8335@users.noreply.github.com>
2023-07-07 12:55:38 +0530

    Update Styling

--- src/components/UserProfile.jsx
+++ src/components/UserProfile.jsx
@@ -7,6 +7,7 @@ import { userProfileData } from "../data/dummy";
 
 const UserProfile = () => {
     const { currentColor } = useStateContext();
+
     return (
         <div className="nav-item absolute right-1 top-16 bg-white dark:bg-[#42464D] p-8 rounded-lg w-96 shadow">
             <div className="flex justify-between items-center">
@@ -19,21 +20,22 @@ const UserProfile = () => {
                     borderRadius="50%"
                 />
             </div>
+
             <div className="flex gap-5 items-center mt-6 border-color border-b-1 pb-6">
                 <img className="rounded-full h-24 w-24" src={avatar} alt="user-profile" />
                 <div>
                     <p className="font-semibold text-xl dark:text-gray-200"> Michael Roberts </p>
                     <p className="text-gray-500 text-sm dark:text-gray-400"> Administrator </p>
                     <p className="text-gray-500 text-sm font-semibold dark:text-gray-400">
-                        info@shop.com{" "}
+                        info@shop.com
                     </p>
-                </div>{" "}
+                </div>
             </div>
             <div>
                 {userProfileData.map((item, index) => (
                     <div
                         key={index}
-                        className="flex gap-5 border-b-1 border-color p-4 hover:bg-light-gray cursor-pointer dark:hover:bg-[#42464D]"
+                        className="flex gap-5 border-b-1 border-color p-4 hover:bg-light-gray cursor-pointer  dark:hover:bg-[#42464D]"
                     >
                         <button
                             type="button"
@@ -42,6 +44,7 @@ const UserProfile = () => {
                         >
                             {item.icon}
                         </button>
+
                         <div>
                             <p className="font-semibold dark:text-gray-200">{item.title}</p>
                             <p className="text-gray-500 text-sm dark:text-gray-400">{item.desc}</p>
@@ -54,8 +57,8 @@ const UserProfile = () => {
                     color="white"
                     bgColor={currentColor}
                     text="Logout"
-                    width="full"
                     borderRadius="10px"
+                    width="full"
                 />
             </div>
         </div>
