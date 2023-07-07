5756ee2372f156f401d5146c558ddb9d3b26949f
Bijay Sharma <58084675+bijaysharma8335@users.noreply.github.com>
2023-07-07 17:10:23 +0530

    Remove unnecessary code

--- src/App.js
+++ src/App.js
@@ -45,7 +45,7 @@ const App = () => {
     }, []);
     return (
         <div className={currentMode === "Dark" ? "dark" : ""}>
-            <BrowserRouter basename="/admin-dashboard">
+            <BrowserRouter >
                 <div className="flex relative dark:bg-main-dark-bg">
                     <div className="fixed right-4 bottom-4" style={{ zIndex: "1000" }}>
                         <TooltipComponent content="Settings" position="Top">
